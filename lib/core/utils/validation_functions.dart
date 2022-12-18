9dd4dfedc4da007730d9d977ad9bb7ff25acd683
RamonPalacio <38841522+RamonPalacio@users.noreply.github.com>
2022-12-18 11:54:17 -0500

    delete validation unnecessary
    se elimina el bloque if ya que no era necesario hacer esa comprobacion si se inicializa la variable isInputStringValid correctamente.

--- lib/core/utils/validation_functions.dart
+++ lib/core/utils/validation_functions.dart
@@ -6,42 +6,33 @@
 ///  length of at least 4
 /// no white space allowed
 bool isValidPassword(String? inputString, {bool isRequired = false}) { 
-bool isInputStringValid = false;
+    bool isInputStringValid = isRequired;
 
-if ((inputString == null ? true : inputString.isEmpty) && !isRequired) {
+    if (inputString != null) {
 
-isInputStringValid = true;
+        const pattern = r'^(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{8,}$';
 
-}
+        final regExp = RegExp(pattern);
 
-if (inputString != null) {
+        isInputStringValid = regExp.hasMatch(inputString) ;
 
-const pattern = r'^(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{8,}$';
+    }
 
-final regExp = RegExp(pattern);
+    return isInputStringValid;
+} /// Checks if string consist only Alphabet. (No Whitespace)
 
-isInputStringValid = regExp.hasMatch(inputString) ;
-
-}
-
-return isInputStringValid; } /// Checks if string consist only Alphabet. (No Whitespace)
 bool isText(String? inputString, {bool isRequired = false}) { 
-bool isInputStringValid = false;
+    bool isInputStringValid = isRequired;
 
-if ((inputString == null ? true : inputString.isEmpty) && !isRequired) {
+    if (inputString != null) {
 
-isInputStringValid = true;
+        const pattern = r'^[a-zA-Z]+$';
 
-}
+        final regExp = RegExp(pattern);
 
-if (inputString != null) {
+        isInputStringValid = regExp.hasMatch(inputString) ;
 
-const pattern = r'^[a-zA-Z]+$';
+    }
 
-final regExp = RegExp(pattern);
-
-isInputStringValid = regExp.hasMatch(inputString) ;
-
-}
-
-return isInputStringValid; } 
+    return isInputStringValid; 
+} 
